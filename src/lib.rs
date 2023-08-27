e43eefc6865755f17cf785310b084055a5718579
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-08-27 19:19:47 +0200

    fix bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{window, Document, console, Element, Node};
+use web_sys::{window, console, Element};
 use std::collections::HashMap;
 use regex::Regex;
 
@@ -47,9 +47,9 @@ pub fn render(script: &str) -> Result<(), JsValue> {
     let document = window.document().expect("should have a document on window");
     let body = document.body().unwrap();
     let lines: Vec<&str> = script.lines().collect();
-    let mut parent_stack: Vec<Element, &str> = vec![body.clone().into(), ""];  // stack of elements, which can be variables/functions (then the name is set)
-    let mut variables: HashMap::new()  // mapping from variable/function name to tuple of Element and places for each argument to be inserted (list of lists)
-    let mut fun_def_args: HashMap::new()
+    let mut parent_stack: Vec<(Element, &str)> = vec![(body.clone().into(), "")];  // stack of elements, which can be variables/functions (then the name is set)
+    let mut variables = HashMap::new();  // mapping from variable/function name to tuple of Element and places for each argument to be inserted (list of lists)
+    // let mut fun_def_args = HashMap::new();
     let mut current_indent = 0;
     
     for line in lines {
@@ -65,19 +65,19 @@ pub fn render(script: &str) -> Result<(), JsValue> {
             return Err("Indentation error: too much indentation at once".into());
         }
         while indent <= current_indent {
-            let (previous_top, var_name) = parent_stack.pop();
+            let (previous_top, var_name) = parent_stack.pop().unwrap();
             current_indent -= 1;
-            if var_name.empty() {
+            if var_name.is_empty() {
                 // append normal elements to their parent. Don't do this for variables/functions
                 parent_stack.last().unwrap().0.append_child(&previous_top)?;
             }
         }
-        raw_line = line.trim();
+        let raw_line = line.trim();
 
         // variable / function
         if raw_line.ends_with("=") {
             let definition = raw_line[0..raw_line.len()-1].trim();
-            let mut name: str = "";
+            let name: &str;
             let mut args: Vec<&str> = vec![];
             // variable
             if definition.split_whitespace().count() == 1 && is_valid_identifier(definition) {
@@ -94,12 +94,12 @@ pub fn render(script: &str) -> Result<(), JsValue> {
             } else {
                 return Err("Syntax error: not a valid variable or function definition".into());
             }
-            let mut arg_places: Vec<Vec<Element>> = vec![];
+            let mut arg_places: Vec<(&str,Vec<Element>)> = vec![];
             for arg_name in args {
                 arg_places.push((arg_name, vec![]));
             }
-            variables.insert(name, arg_places);
-            var_elem = document.create_element("div")?;
+            let var_elem = document.create_element("div")?;
+            variables.insert(name, (var_elem.clone(), arg_places));
             parent_stack.push((var_elem, name));
             current_indent += 1;
 
@@ -122,7 +122,7 @@ pub fn render(script: &str) -> Result<(), JsValue> {
             let re = Regex::new(r#"(?P<name>\w+)="(?P<value>[^"]+)""#).unwrap();
             
             // Set each attribute on the element
-            for cap in re.captures_iter(attributes) {
+            for cap in re.captures_iter(&raw_line[tag_name.len()..].trim()) {
                 let name = &cap["name"];
                 let value = &cap["value"];
                 element.set_attribute(name, value)?;
@@ -131,26 +131,26 @@ pub fn render(script: &str) -> Result<(), JsValue> {
             current_indent += 1;
 
         // variable
-        } else if raw_line.starts_with("$") && is_valid_identifier(&raw_line.split("(").next()[1..]) {
+        } else if raw_line.starts_with("$") && is_valid_identifier(&raw_line.split("(").next().unwrap()[1..]) {
             let name = &raw_line.split("(").next().unwrap()[1..];
             if variables.contains_key(name) {
                 // make sure there are no references to variables that are still being defined:
-                for (_, x) in parent_stack {
-                    if x == name {
+                for (_, x) in parent_stack.as_slice() {
+                    if *x == name {
                         return Err("Syntax error: variable is still being defined".into());
                     }
                 }
-                let (var, arg_places) = variables.get(name);
+                let (var, arg_places) = variables.get(name).unwrap();
                 // function call
                 if arg_places.len() > 0 {
                     if raw_line.split("(").count() != 2 {
-                        return Err("Syntax Error: invalid function call")
+                        return Err("Syntax Error: invalid function call".into());
                     }
                     // get given args
-                    let given_args_raw = raw_line.split("(").nth(1).unwrap().split(")").next().unwrap().split(",").collect();
+                    let given_args_raw : Vec<&str> = raw_line.split("(").nth(1).unwrap().split(")").next().unwrap().split(",").collect();
                     let mut given_args: Vec<Element> = vec![];
                     for arg_raw in given_args_raw {
-                        let arg = arg_raw.trim()
+                        let arg = arg_raw.trim();
                         // string
                         if arg.starts_with("\"") && arg.ends_with("\"") {
                             // create span element with string inside
@@ -159,20 +159,20 @@ pub fn render(script: &str) -> Result<(), JsValue> {
                             given_args.push(element);
                         } else if arg.starts_with("$") {
                             // insert variable element
-                            let (arg_var, arg_var_arg_places) = variables.get(name);
-                            if !arg_var_arg_places.empty() {
-                                return Err("Syntax Error: Function calls inside other function calls are not allowed");
+                            let (arg_var, arg_var_arg_places) = variables.get(name).unwrap();
+                            if !arg_var_arg_places.is_empty() {
+                                return Err("Syntax Error: Function calls inside other function calls are not allowed".into());
                             }
                             let cloned_given_arg = arg_var.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
                             given_args.push(cloned_given_arg);
                         } else {
-                            return Err("Syntax Error: unknown argument type");
+                            return Err("Syntax Error: unknown argument type".into());
                         }
                     }
                     // insert args into place
                     for ((_, arg_place), given_arg) in arg_places.iter().zip(given_args.iter()) {
                         for place in arg_place {
-                            place.append_child(given_arg);
+                            let _ = place.append_child(given_arg);
                         }
                     }
                     // create a copy of the prepared function body
@@ -180,7 +180,7 @@ pub fn render(script: &str) -> Result<(), JsValue> {
                     // undo inserting args, so the function is ready for the next time
                     for (_, arg_place) in arg_places {
                         for place in arg_place {
-                            place.pop_child();
+                            let _ = place.remove_child(&place.last_child().unwrap());
                         }
                     }
                     // add new prepared and cloned function element to the stack
