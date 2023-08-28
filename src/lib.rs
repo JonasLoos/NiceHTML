0a3234f2e6bff08bf86973591eec65a81e7c6a9b
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-08-28 23:19:22 +0200

    fix bugs and add comments

--- src/lib.rs
+++ src/lib.rs
@@ -42,8 +42,8 @@ fn is_valid_identifier(s: &str) -> bool {
     chars.all(|c| c.is_ascii_alphanumeric() || c == '_')
 }
 
-fn err(msg: &str, line: &str) -> Result<(), JsValue> {
-    return Err(format!("NiceHTML Error: {}\n  for line: `{}`", msg, line).into())
+fn err(msg: &str, line_nr: u32, line: &str) -> Result<(), JsValue> {
+    return Err(format!("NiceHTML Error: {}\nline {}: `{}`", msg, line_nr, line).into())
 }
 
 #[wasm_bindgen]
@@ -57,11 +57,11 @@ pub fn render(script: &str) -> Result<(), JsValue> {
     let body = document.body().unwrap();
     let lines: Vec<&str> = script.lines().collect();
     let mut parent_stack: Vec<(Element, &str)> = vec![(body.clone().into(), "")];  // stack of elements, which can be variables/functions (then the name is set)
-    let mut variables = HashMap::new();  // mapping from variable/function name to tuple of Element and places for each argument to be inserted (list of lists)
-    // let mut fun_def_args = HashMap::new();
+    let mut variables: HashMap<&str, (Element, Vec<(&str, Vec<String>)>)> = HashMap::new();  // mapping from variable/function name to tuple of Element and places for each argument to be inserted (list of lists)
+    let mut next_placeholder_id: i32 = 0;
     let mut current_indent = 0;
     
-    for line in lines {
+    for (line, line_nr) in lines.iter().zip(1..) {
         // ignore empty lines and comments
         if line.trim().is_empty() || line.trim().starts_with("#") {
             continue;
@@ -71,42 +71,55 @@ pub fn render(script: &str) -> Result<(), JsValue> {
         
         let indent = line.chars().take_while(|&c| c == ' ').count() / 4 + 1;
         if indent > current_indent + 1 {
-            return err("too much indentation at once", line);
+            return err("too much indentation at once", line_nr, line);
         }
         while indent <= current_indent {
-            let (previous_top, var_name) = parent_stack.pop().unwrap();
+            let (finished_element, var_name) = parent_stack.pop().unwrap();
+            // console::log_1(&format!("finished element {}", finished_element.outer_html()).into());
             current_indent -= 1;
             if var_name.is_empty() {
-                // append normal elements to their parent. Don't do this for variables/functions
-                parent_stack.last().unwrap().0.append_child(&previous_top)?;
+                // append normal elements to their parent. Don't do this for variables/functions (for which `var_name` is set)
+                parent_stack.last().unwrap().0.append_child(&finished_element)?;
+            } else {
+                // overwrite the dummy variable element with the new finished element
+                let prev_var_entry = variables.remove(var_name).unwrap();
+                variables.insert(var_name, (finished_element, prev_var_entry.1));
             }
         }
         let raw_line = line.trim();
 
-        // variable / function
+        // assignment of variable / function
         if raw_line.ends_with("=") {
             let definition = raw_line[0..raw_line.len()-1].trim();
             let name: &str;
             let mut args: Vec<&str> = vec![];
+
             // variable
             if definition.split_whitespace().count() == 1 && is_valid_identifier(definition) {
                 name = definition;
+
+            // function
             } else if definition.contains("(") && definition.contains(")") {
                 name = definition.split("(").next().unwrap();
                 if !is_valid_identifier(name) {
-                    return err("not a valid variable or function definition", line);
+                    return err("not a valid variable or function definition", line_nr, line);
                 }
                 if !definition.split("(").nth(1).unwrap().split(")").nth(1).unwrap().trim().is_empty() {
-                    return err("not a valid variable or function definition", line);
+                    return err("not a valid variable or function definition", line_nr, line);
                 }
                 args = definition.split("(").nth(1).unwrap().split(")").next().unwrap().split(",").collect();
+
             } else {
-                return err("not a valid variable or function definition", line);
+                return err("not a valid variable or function definition", line_nr, line);
             }
-            let mut arg_places: Vec<(&str,Vec<Element>)> = vec![];
+
+            // initialize the argument places arrayws for the function
+            let mut arg_places: Vec<(&str,Vec<String>)> = vec![];
             for arg_name in args {
-                arg_places.push((arg_name, vec![]));
+                arg_places.push((arg_name.trim(), vec![]));
             }
+
+            // create div where the function content is put into
             let var_elem = document.create_element("div")?;
             variables.insert(name, (var_elem.clone(), arg_places));
             parent_stack.push((var_elem, name));
@@ -139,39 +152,47 @@ pub fn render(script: &str) -> Result<(), JsValue> {
             parent_stack.push((element, ""));
             current_indent += 1;
 
-        // variable
+        // variable access / function call
         } else if raw_line.starts_with("$") && is_valid_identifier(&raw_line.split("(").next().unwrap()[1..]) {
-            let name = &raw_line.split("(").next().unwrap()[1..];
+            let name = raw_line.split("(").next().unwrap()[1..].trim();
+            let new_element: Element;
 
             // if this is inside a function definition and an argument is accessed, create an empty div as placeholder
-            let mut is_arg = false;
+            let mut placeholder: Option<Element> = None;
             for (_, parent_name) in parent_stack.iter().rev() {
                 if let Some((_, parent_arg_places)) = variables.get_mut(parent_name) {
                     // if parent is a function
                     if !parent_arg_places.is_empty() {
-                        // add a placeholder for the argument
-                        let placeholder = document.create_element("div")?;
-                        parent_arg_places[0].1.push(placeholder);
-                        is_arg = true;
-                        break;
+                        // if the variable is an argument of the function, insert a placeholder
+                        if parent_arg_places.iter().any(|x| x.0 == name) {
+                            let placeholder_id = format!("nicehtmlID-{}-{}-{}", parent_name, name, next_placeholder_id);
+                            let placeholder_elem = document.create_element("div")?;
+                            placeholder_elem.set_attribute("id", &placeholder_id)?;
+                            let arg_pos = parent_arg_places.iter().position(|x| x.0 == name).unwrap();
+                            parent_arg_places[arg_pos].1.push(placeholder_id);
+                            next_placeholder_id += 1;
+                            placeholder = Some(placeholder_elem);
+                            break;
+                        }
                     }
                 }
             }
 
-            if is_arg {
-                // already handled above
+            if placeholder.is_some() {
+                // the found variable is an argument, so use the placeholder
+                new_element = placeholder.unwrap();
             } else if variables.contains_key(name) {
                 // make sure there are no references to variables that are still being defined:
                 for (_, x) in parent_stack.as_slice() {
                     if *x == name {
-                        return err("variable is still being defined", line);
+                        return err("variable is still being defined", line_nr, line);
                     }
                 }
                 let (_, arg_places) = variables.get(name).unwrap();
                 // function call
                 if arg_places.len() > 0 {
                     if raw_line.split("(").count() != 2 {
-                        return err("invalid function call", line);
+                        return err("invalid function call", line_nr, line);
                     }
                     // get given args
                     let given_args_raw : Vec<&str> = raw_line.split("(").nth(1).unwrap().split(")").next().unwrap().split(",").collect();
@@ -187,47 +208,45 @@ pub fn render(script: &str) -> Result<(), JsValue> {
                         } else if arg.starts_with("$") {
                             // insert variable element
                             let (arg_var, arg_var_arg_places) = variables.get(name).unwrap();  // TODO: not only check variables, but also function arguments
-                            if !arg_var_arg_places.is_empty() {
-                                return err("Function calls inside other function calls are not allowed", line);
+                            if !arg_var_arg_places.iter().any(|x| x.1.len() > 0) {
+                                return err("Function calls inside other function calls are not allowed", line_nr, line);
                             }
                             let cloned_given_arg = arg_var.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
                             given_args.push(cloned_given_arg);
                         } else {
-                            return err("unknown argument type", line);
+                            return err(&format!("unknown argument type for `{}`", arg), line_nr, line);
                         }
                     }
+
+                    // create a copy of the function body
+                    new_element = variables.get(name).unwrap().0.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
+
                     // insert args into place
                     for ((_, arg_place), given_arg) in arg_places.iter().zip(given_args.iter()) {
                         for place in arg_place {
-                            let _ = place.append_child(&given_arg.clone_node().unwrap());
+                            // search for the placeholder with the correct id and insert the argument there
+                            if let Some(placeholder_elem) = new_element.query_selector(&format!("#{}",place))? {
+                                placeholder_elem.append_child(&given_arg.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap())?;
+                            } else {
+                                // internal error, this should not happen
+                                return err(&format!("placeholder (`{}`) not found in element `{}`", place, new_element.inner_html()), line_nr, line);
+                            }
                         }
                     }
-                    
-                    // create a copy of the prepared function body
-                    // undo inserting args, so the function is ready for the next time
-                    for (_, arg_place) in arg_places {
-                        for place in arg_place {
-                            let _ = place.remove_child(&place.last_child().unwrap());
-                        }
-                    }
-                    let new_element = variables.get(name).unwrap().0.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
-                    // add new prepared and cloned function element to the stack
-                    parent_stack.push((new_element, ""));
+
                 // variable access
                 } else {
-                    if !is_arg {
-                        let new_element = variables.get(name).unwrap().0.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
-                        parent_stack.push((new_element, ""));
-                    }
+                    new_element = variables.get(name).unwrap().0.clone_node_with_deep(true).unwrap().dyn_into::<web_sys::Element>().unwrap();
                 }
-                current_indent += 1;
             } else {
-                return err("Variable not defined", line);
+                return err("Variable not defined", line_nr, line);
             }
+            parent_stack.push((new_element, ""));
+            current_indent += 1;
 
         // error
         } else {
-            return err("Not sure that this line is", line);
+            return err("Not sure what this line is", line_nr, line);
         }
     }
     // pop all remaining elements (except body) from the stack
