e417069cae78690414e0d40025bcd87749677b98
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2023-09-02 23:05:10 +0200

    move files and add html and js to git

---
+++ README.md
@@ -0,0 +1,5 @@
+# NiceHTML
+
+NiceHTML is a language designed for building web pages, very similar to html, but with a cleaner and more intuitive syntax.
+
+The transpiler is written in rust and called using wasm to transpile nicehtml code to html and insert it into the DOM.
---
+++ nicehtml.js
@@ -0,0 +1,24 @@
+import * as wasm from './nicehtml_transpiler/pkg/web_compiler.js';
+// Start loading the WASM module
+const wasmPromise = wasm.default();
+
+// Fetch the scripts
+const scripts = document.querySelectorAll('script[type="text/nicehtml"]');
+const scriptPromises = Array.from(scripts).map((script) => {
+    if (script.src) {
+        // Fetch the script content
+        return fetch(script.src + '?timestamp=' + Date.now())
+            .then(response => response.text())
+            .catch(error => console.error('Error loading script:', error));
+    } else {
+        // Use the inline script content
+        return Promise.resolve(script.textContent);
+    }
+});
+
+// Wait for the WASM module and scripts to load
+Promise.all([wasmPromise, ...scriptPromises])
+    .then(([wasmModule, ...scriptContents]) => {
+        console.log('Start rendering scripts');
+        scriptContents.forEach(content => wasm.transpile(content));
+    });
